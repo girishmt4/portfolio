bde075023db59732601517c90fbd00a22dc20754
Girish Tiwale <girishmt4@gmail.com>
2024-07-09 21:08:27 -0400

    icon changes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
       </Head>
       <body>
         <Main />
