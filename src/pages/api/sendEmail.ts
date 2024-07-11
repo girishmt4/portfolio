45e0060d7db5f37770c0aa7d3f7eb931328f2449
Girish Tiwale <girishmt4@gmail.com>
2024-07-11 16:25:05 -0400

    unsubscribe headers added

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -31,30 +31,30 @@ export default async function handler(
       text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
       html: `
         <html>
-        <head>
-          <style>
-            body { font-family: Arial, sans-serif; line-height: 1.6; }
-            .container { width: 80%; margin: auto; padding: 20px; }
-            .header { text-align: center; padding: 10px 0; border-bottom: 1px solid #eaeaea; }
-            .content { margin: 20px 0; }
-            .footer { text-align: center; padding: 10px 0; border-top: 1px solid #eaeaea; font-size: 0.9em; color: #777; }
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <div class="header"><h2>Contact Form Submission</h2></div>
-            <div class="content">
-              <p>Name: ${name}</p>
-              <p>Email: ${email}</p>
-              <p>Message:</p>
-              <p>${message}</p>
-            </div>
-            <div class="footer">
-              <p>Received via girishtiwale.com</p>
-            </div>
-          </div>
-        </body>
-        </html>
+<head>
+  <style>
+    body { font-family: Arial, sans-serif; line-height: 1.6; }
+    .container { width: 80%; margin: auto; padding: 20px; }
+    .header { text-align: center; padding: 10px 0; border-bottom: 1px solid #eaeaea; }
+    .content { margin: 20px 0; }
+    .footer { text-align: center; padding: 10px 0; border-top: 1px solid #eaeaea; font-size: 0.9em; color: #777; }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header"><h2>Contact Form Submission</h2></div>
+    <div class="content">
+      <p>Name: ${name}</p>
+      <p>Email: ${email}</p>
+      <p>Message:</p>
+      <p>${message}</p>
+    </div>
+    <div class="footer">
+      <p>Received via girishtiwale.com</p>
+    </div>
+  </div>
+</body>
+</html>
       `,
     };
 
@@ -65,36 +65,39 @@ export default async function handler(
       subject: "Thank you for contacting me!",
       text: "Thank you for contacting me. I will get back to you!",
       html: `
-        <html>
-        <head>
-          <style>
-            body { font-family: Arial, sans-serif; line-height: 1.6; }
-            .container { width: 80%; margin: auto; padding: 20px; }
-            .header { text-align: center; padding: 10px 0; border-bottom: 1px solid #eaeaea; }
-            .content { margin: 20px 0; }
-            .footer { text-align: center; padding: 10px 0; border-top: 1px solid #eaeaea; font-size: 0.9em; color: #777; }
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <div class="header"><h2>Thank You for Contacting Me!</h2></div>
-            <div class="content">
-              <p>Hi ${name},</p>
-              <p>Thank you for reaching out to me. I have received your message and will get back to you as soon as possible.</p>
-              <p>If you have any further questions, please feel free to reply to this email.</p>
-              <br>
-              <p>Best regards,</p>
-              <p>Girish Tiwale</p>
-              <p>Full Stack Developer</p>
-              <p><a href="https://girishtiwale.com">girishtiwale.com</a></p>
-            </div>
-            <div class="footer">
-              <p>Girish Tiwale</p>
-            </div>
-          </div>
-        </body>
-        </html>
+          <html>
+<head>
+  <style>
+    body { font-family: Arial, sans-serif; line-height: 1.6; }
+    .container { width: 80%; margin: auto; padding: 20px; }
+    .header { text-align: center; padding: 10px 0; border-bottom: 1px solid #eaeaea; }
+    .content { margin: 20px 0; }
+    .footer { text-align: center; padding: 10px 0; border-top: 1px solid #eaeaea; font-size: 0.9em; color: #777; }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header"><h2>Thank You for Contacting Me!</h2></div>
+    <div class="content">
+      <p>Hi ${name},</p>
+      <p>Thank you for reaching out to me. I have received your message and will get back to you as soon as possible.</p>
+      <p>If you have any further questions, please feel free to reply to this email.</p>
+      <br>
+      <p>Best regards,</p>
+      <p>Girish Tiwale</p>
+      <p>Full Stack Developer</p>
+      <p><a href="https://girishtiwale.com">Portfolio</a></p>
+    </div>
+    <div class="footer">
+      <p>Girish Tiwale</p>
+    </div>
+  </div>
+</body>
+</html>
       `,
+      headers: {
+        "List-Unsubscribe": "<mailto:unsubscribe@girishtiwale.com>",
+      },
     };
 
     try {
